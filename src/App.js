4bea520587c2b98d34e454f94d87cb78933d9d7b
Yuns <hope4_u@naver.com>
2023-01-30 17:02:01 +0900

    Build the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import styled from "styled-components";
 
-function App() {
+// Pages
+import MainPage from "./component/page/MainPage";
+import PostViewPage from './component/page/PostViewPage';
+import PostWritePage from './component/page/PostWritePage';
+
+require('react-dom');
+window.React2 = require('react');
+console.log(window.React1 === window.React2);
+
+const MainTitleText = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<MainPage />} />
+        <Route path="post-write" element={<PostWritePage />} />
+        <Route path="post/:postId" element={<PostViewPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
